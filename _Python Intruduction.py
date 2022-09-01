f83753c882daf4b959f9011ea71d66564c05e33a
ramu <ramu@gmail.com>
2022-09-01 14:41:52 +0530

    1st

---
+++ _Python Intruduction.py
@@ -0,0 +1,86 @@
+#Python Introduction
+
+#Python History –
+Python was invented by Guido van Rossum in 1991.
+
+#What is Python: -
+1)	Python is a simple, general purpose, high level, and object-oriented programming language.
+2)	Python is an interpreted scripting language. Which makes it attractive for Application Development.
+3)	Python supports multiple programming pattern, including object-oriented, imperative, and functional or
+procedural programming styles.
+
+#Python New Versions – Python 2 vs Python 3
+
+Differences – Python 2 is uses print as a statement and used as print “something” to print the some string.
+Python 3 is uses print as a statement and used as print (“something”).
+
+#Why Python – Python provides many useful features to the programmer.
+Easy to use and Learn
+Interpreted Language
+Object-Oriented Language
+Open Source Language
+GUI Programming Support
+Embeddable
+
+#Where is Python used –?
+
+Python is a general-purpose, popular programming language and it is used in almost every technical field.
+Data Science
+Date Mining
+Desktop Applications
+Mobile Applications
+Software Development
+Artificial Intelligence
+Web Applications
+Machine Learning
+
+#Why they called python is an Interpreter Language
+
+There are no separate compilation and execution steps like C and C++.
+1)Directly run the program from the source code.
+2)Internally, Python converts the source code into an intermediate form called byte codes which is then translated into
+native language of specific computer to run it.
+3)No need to worry about linking and loading with libraries, etc
+
+#Features –
+
+1)	Interpreted
+2)	Platform Independent
+3)	Free and Open Source
+4)	High-level Programming
+5)	Simple
+6)	Embeddable
+7)	Robust
+
+#Advantages –
+1)	Open source and community development
+2)	Versatile, Easy to read, learn and write
+3)	User-friendly data structures
+4)	High-level language
+5)	Dynamically typed language
+
+#Disadvantages –
+1)	Slow speed of execution compared to C, C++
+2)	Irritating for C, C++ Programmers.
+
+#Compile Time vs Run Time
+Compile time - Compile time is the period when the programming code (such as C#, Java, C, Python) is converted to the
+machine code. (Binary Code). – Statically Typed language.
+Run Time - Runtime is the period of time when a program is running and generally occurs after compile time.
+– Dynamically Typed language.
+
+#Interpreted vs Compiled time programming languages. Explain in detail
+
+A compiled language is converted into machine code so that the processor can execute it. An interpreted language is a
+language in which the implementations execute instructions directly without earlier compiling a program into machine
+language. The compiled programs run faster than interpreted programs.
+
+# .py vs .pyc files
+
+.PY files contain the source code of a program,
+.PYC file contains the byte code of your program
+
+Flavors of Python –
+1)	CPython – It is the standards flavor of Python. It can be used to work with C language Application
+2)	PyPy – JIT Compiler is available inside PVM
+3)	Anaconda – It will handled large volume of data processing
